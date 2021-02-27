3c59bb505575ba73d9f0cf85862ebe504574198e
Lương Công Hiếu <luonghieu184@gmail.com>
2021-02-27 10:21:49 +0700

    replace

---
+++ content/promoted/coffee-song-trang-hot-20201.md
@@ -0,0 +1,8 @@
+---
+title: "Coffee Song Trang Hot 2021 Promoted"
+date: 2021-02-26T22:42:21+07:00
+draft: false
+image: ""
+
+---
+
--- themes/cfgarden/static/js/osahan.js
+++ themes/cfgarden/static/js/osahan.js
@@ -188,3 +188,18 @@ Version: 1.0
     var Nav = $main_nav.hcOffcanvasNav(defaultOptions);
 
 })(jQuery); // End of use strict
+$(document).ready(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        var offset = -100; // <-- change the value here
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top + offset
+                }, 800);
+                return false;
+            }
+        }
+    });
+  });
